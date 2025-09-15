4397fa2e66e6d0715acd602556081df2bda05808
Myenum <201346485+Myenum2412@users.noreply.github.com>
2025-09-15 13:25:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  images:{
-    remotePatterns:[
+  experimental: {
+    middleware: true,
+  },
+  images: {
+    remotePatterns: [
       {
         protocol: 'https',
-        hostname:"*"
-      }
-    ]    
-  }
+        hostname: '*',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
-
-
